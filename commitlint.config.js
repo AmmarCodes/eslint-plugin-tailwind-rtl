4121ee510b562f1e2b58ec54371d4e0c32bd0b33
Ammar Alakkad <am.alakkad@gmail.com>
2026-05-25 13:41:34 +0300

    chore(lint): add commitlint configuration
    Enforce Conventional Commits format in CI.
    Ignore merge commits and Dependabot commits.

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  ignores: [
+    (message) => message.startsWith("Merge"),
+    (message) => /^chore\(deps\)/.test(message),
+  ],
+};
